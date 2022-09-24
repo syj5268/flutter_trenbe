c01c25e9cde380de74e8cdca22c8cceb2db33915
syj5268 <syj5268@korea.ac.kr>
2022-09-24 12:47:33 +0900

    bottomnavigationbar

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,59 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: PreferredSize(
-                preferredSize: Size.fromHeight(30.0),
-                child: AppBar(
-                  title: Image.asset("assets/images/logo.png"),
-                  actions: [
-                    IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.favorite_border_outlined)),
-                    IconButton(onPressed: () {}, icon: Icon(Icons.shop))
-                  ],
-                ))));
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(50.0),
+          child: AppBar(
+            title: Image.asset("assets/images/logo.png", width: 100),
+            backgroundColor: Colors.white,
+            actions: [
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.favorite_border_outlined),
+                  color: Colors.black),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.shop_outlined),
+                  color: Colors.black),
+              SizedBox(width: 10)
+            ],
+          )),
+      body: Text('hello'),
+      bottomNavigationBar: buttonchange(),
+    ));
+  }
+}
+
+class buttonchange extends StatefulWidget {
+  const buttonchange({super.key});
+
+  @override
+  State<buttonchange> createState() => _buttonchangeState();
+}
+
+class _buttonchangeState extends State<buttonchange> {
+  int _selectedIndex = 2;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: _selectedIndex,
+      unselectedItemColor: Colors.grey,
+      selectedItemColor: Colors.purple,
+      onTap: _onItemTapped,
+      showUnselectedLabels: true,
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.category), label: '카테고리'),
+        BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: '이벤트'),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
+        BottomNavigationBarItem(icon: Icon(Icons.shop_two), label: '뉴 시즌'),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: '마이'),
+      ],
+    );
   }
 }
